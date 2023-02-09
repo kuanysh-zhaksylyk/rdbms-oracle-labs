512d1e42b22b5d3d3974093f6512ea994edc4690
kuanysh-zhaksylyk <kuan.zhk@gmail.com>
2023-02-09 22:43:20 +0300

    Fix(Common): fixing coding ed_db.sql file

--- ed_db.sql
+++ ed_db.sql
@@ -1,187 +1,470 @@
-CREATE TABLE branch(bno INTEGER NOT NULL,
-                    street VARCHAR2(30) NOT NULL,
-                    city VARCHAR2(15) NOT NULL,
-                    tel_no VARCHAR2(17),
-                    PRIMARY KEY(bno),
-                    CONSTRAINT ch_telno_branch
-                                CHECK(REGEXP_LIKE(tel_no, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
-                    CONSTRAINT uni_telno_branch UNIQUE(tel_no));
+CREATE TABLE BRANCH(
+    BNO INTEGER NOT NULL,
+    STREET VARCHAR2(30) NOT NULL,
+    CITY VARCHAR2(15) NOT NULL,
+    TEL_NO VARCHAR2(17),
+    PRIMARY KEY(BNO),
+    CONSTRAINT CH_TELNO_BRANCH CHECK(REGEXP_LIKE(TEL_NO, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
+    CONSTRAINT UNI_TELNO_BRANCH UNIQUE(TEL_NO)
+);
 
-CREATE TABLE staff(sno INTEGER NOT NULL,
-                    fname VARCHAR2(20) NOT NULL,
-                    lname VARCHAR2(20) NOT NULL,
-                    address VARCHAR2(60) NOT NULL,
-                    tel_no VARCHAR2(17) NOT NULL,
-                    position VARCHAR2(40) NOT NULL,
-                    sex VARCHAR2(6),
-                    dob DATE,
-                    salary NUMBER(5,2),
-                    bno INTEGER,
-                    PRIMARY KEY(sno),
-                    CONSTRAINT ch_telno_staff
-                               CHECK(REGEXP_LIKE(tel_no, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
-                    CONSTRAINT uni_telno_staff 
-                               UNIQUE(tel_no),
-                    CONSTRAINT ch_sex
-                               CHECK(sex IN('male','female')),
-                    CONSTRAINT bno_fk_staff
-                               FOREIGN KEY(bno)
-                               REFERENCES branch
-                     );
+CREATE TABLE STAFF(
+    SNO INTEGER NOT NULL,
+    FNAME VARCHAR2(20) NOT NULL,
+    LNAME VARCHAR2(20) NOT NULL,
+    ADDRESS VARCHAR2(60) NOT NULL,
+    TEL_NO VARCHAR2(17) NOT NULL,
+    POSITION VARCHAR2(40) NOT NULL,
+    SEX VARCHAR2(6),
+    DOB DATE,
+    SALARY NUMBER(5, 2),
+    BNO INTEGER,
+    PRIMARY KEY(SNO),
+    CONSTRAINT CH_TELNO_STAFF CHECK(REGEXP_LIKE(TEL_NO, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
+    CONSTRAINT UNI_TELNO_STAFF UNIQUE(TEL_NO),
+    CONSTRAINT CH_SEX CHECK(SEX IN('male', 'female')),
+    CONSTRAINT BNO_FK_STAFF FOREIGN KEY(BNO) REFERENCES BRANCH
+);
 
-CREATE TABLE property_for_rent(pno INTEGER NOT NULL,
-                    street VARCHAR2(30) NOT NULL,
-                    city VARCHAR2(15) NOT NULL,
-                    type_obj CHAR(1) NOT NULL,
-                    rooms INTEGER,
-                    rent NUMBER(5,2),
-                    ono INTEGER,
-                    sno INTEGER,
-                    bno INTEGER,
-                    PRIMARY KEY(pno),
-                    CONSTRAINT ch_obj_type
-                               CHECK(type_obj IN('h','f')),
-                    CONSTRAINT sno_fk_pfrent
-                               FOREIGN KEY(sno)
-                               REFERENCES staff,           
-                    CONSTRAINT bno_fk_pfrent
-                               FOREIGN KEY(bno)
-                               REFERENCES branch
-                     );
+CREATE TABLE PROPERTY_FOR_RENT(
+    PNO INTEGER NOT NULL,
+    STREET VARCHAR2(30) NOT NULL,
+    CITY VARCHAR2(15) NOT NULL,
+    TYPE_OBJ CHAR(1) NOT NULL,
+    ROOMS INTEGER,
+    RENT NUMBER(5, 2),
+    ONO INTEGER,
+    SNO INTEGER,
+    BNO INTEGER,
+    PRIMARY KEY(PNO),
+    CONSTRAINT CH_OBJ_TYPE CHECK(TYPE_OBJ IN('h', 'f')),
+    CONSTRAINT SNO_FK_PFRENT FOREIGN KEY(SNO) REFERENCES STAFF,
+    CONSTRAINT BNO_FK_PFRENT FOREIGN KEY(BNO) REFERENCES BRANCH
+);
 
-CREATE SYNONYM objects for property_for_rent;
+CREATE SYNONYM OBJECTS FOR PROPERTY_FOR_RENT;
 
-CREATE TABLE renter(rno INTEGER NOT NULL,
-                    fname VARCHAR2(20) NOT NULL,
-                    lname VARCHAR2(20),
-                    address VARCHAR2(60),
-                    tel_no VARCHAR2(17),
-                    pref_type CHAR(1),
-                    max_rent NUMBER(5,2),
-                    bno INTEGER,
-                    PRIMARY KEY(rno),
-                    CONSTRAINT ch_pref_type
-                               CHECK(pref_type IN('h','f')),
-                    CONSTRAINT ch_telno_renter
-                                CHECK(REGEXP_LIKE(tel_no, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
-                    CONSTRAINT uni_telno_renter
-                               UNIQUE(tel_no),                                    
-                    CONSTRAINT bno_fk_renter
-                               FOREIGN KEY(bno)
-                               REFERENCES branch
-                     );
+CREATE TABLE RENTER(
+    RNO INTEGER NOT NULL,
+    FNAME VARCHAR2(20) NOT NULL,
+    LNAME VARCHAR2(20),
+    ADDRESS VARCHAR2(60),
+    TEL_NO VARCHAR2(17),
+    PREF_TYPE CHAR(1),
+    MAX_RENT NUMBER(5, 2),
+    BNO INTEGER,
+    PRIMARY KEY(RNO),
+    CONSTRAINT CH_PREF_TYPE CHECK(PREF_TYPE IN('h', 'f')),
+    CONSTRAINT CH_TELNO_RENTER CHECK(REGEXP_LIKE(TEL_NO, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
+    CONSTRAINT UNI_TELNO_RENTER UNIQUE(TEL_NO),
+    CONSTRAINT BNO_FK_RENTER FOREIGN KEY(BNO) REFERENCES BRANCH
+);
 
-CREATE TABLE owner(ono INTEGER NOT NULL,
-                    fname VARCHAR2(20),
-                    lname VARCHAR2(20),
-                    address VARCHAR2(60),
-                    tel_no VARCHAR2(17),
-                    PRIMARY KEY(ono),
-                    CONSTRAINT ch_telno_owner
-                                CHECK(REGEXP_LIKE(tel_no, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
-                    CONSTRAINT uni_telno_owner
-                               UNIQUE(tel_no)                                   
-                     ); 
+CREATE TABLE OWNER(
+    ONO INTEGER NOT NULL,
+    FNAME VARCHAR2(20),
+    LNAME VARCHAR2(20),
+    ADDRESS VARCHAR2(60),
+    TEL_NO VARCHAR2(17),
+    PRIMARY KEY(ONO),
+    CONSTRAINT CH_TELNO_OWNER CHECK(REGEXP_LIKE(TEL_NO, '^\+375\(\d{2}\)\d{3}-\d{2}-\d{2}$')),
+    CONSTRAINT UNI_TELNO_OWNER UNIQUE(TEL_NO)
+);
 
-ALTER TABLE property_for_rent
-ADD CONSTRAINT ono_fk1
-               FOREIGN KEY(ono)
-               REFERENCES owner;
+ALTER TABLE PROPERTY_FOR_RENT ADD CONSTRAINT ONO_FK1 FOREIGN KEY(ONO) REFERENCES OWNER;
 
-CREATE TABLE viewing(rno INTEGER NOT NULL,
-                     pno INTEGER NOT NULL,
-                    date1 DATE NOT NULL,
-                    comment1 VARCHAR2(300),
-                    PRIMARY KEY(rno,pno),
-                    CONSTRAINT rno_fk_viewing
-                               FOREIGN KEY(rno)
-                               REFERENCES renter,
-                    CONSTRAINT pno_fk_viewing
-                               FOREIGN KEY(pno)
-                               REFERENCES property_for_rent                             
-                     );      
+CREATE TABLE VIEWING(
+    RNO INTEGER NOT NULL,
+    PNO INTEGER NOT NULL,
+    DATE1 DATE NOT NULL,
+    COMMENT1 VARCHAR2(300),
+    PRIMARY KEY(RNO, PNO),
+    CONSTRAINT RNO_FK_VIEWING FOREIGN KEY(RNO) REFERENCES RENTER,
+    CONSTRAINT PNO_FK_VIEWING FOREIGN KEY(PNO) REFERENCES PROPERTY_FOR_RENT
+);
 
-╧юёыхфютрЄхы№эюёЄш:
-CREATE SEQUENCE branch_seq
-START WITH 1
-INCREMENT BY 1;
 
-CREATE SEQUENCE staff_seq
-START WITH 1
-INCREMENT BY 1;
+CREATE SEQUENCE BRANCH_SEQ START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE property_for_rent_seq
-START WITH 1
-INCREMENT BY 1;
+CREATE SEQUENCE STAFF_SEQ START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE renter_seq
-START WITH 1
-INCREMENT BY 1;
+CREATE SEQUENCE PROPERTY_FOR_RENT_SEQ START WITH 1 INCREMENT BY 1;
 
-CREATE OR REPLACE TRIGGER seq_branch
-	BEFORE INSERT ON branch
-	FOR EACH ROW
-	BEGIN	
-	SELECT branch_seq.nextval 
-		INTO :NEW.bno 
-	FROM DUAL;
-END seq_branch ;
+CREATE SEQUENCE RENTER_SEQ START WITH 1 INCREMENT BY 1;
 
-begin
-insert into branch(street, city, tel_no) values('╦хэшэр 42','╠шэёъ','+375(29)221-11-71');
-insert into branch(street, city, tel_no) values('▀ъєср ╩юырёр 13','╠шэёъ','+375(33)222-11-71');
-insert into branch(street, city, tel_no) values('╧ЁюыхЄрЁёър  17','┴ЁхёЄ','+375(25)223-11-71');
-insert into branch (street, city, tel_no)values('╤ютхЎър ','┴ЁхёЄ','+375(29)224-11-71');
-insert into branch (street, city, tel_no)values('╩юьёюьюы№ёър  764','┬шЄхсёъ','+375(29)225-11-71');
-insert into branch (street, city, tel_no)values('▀эъш ╩єяры√ 123','├юьхы№','+375(29)226-11-71');
-insert into branch(street, city, tel_no) values('╠ръёшьр ╥рэър 64','├Ёюфэю','+375(29)227-11-71');
-insert into branch (street, city, tel_no)values('╦хэшэр 88','╠юушыхт','+375(29)228-11-71');
-insert into branch (street, city, tel_no)values('╠р°хЁютр 44','╠юушыхт','+375(33)229-11-71');
-insert into branch (street, city, tel_no)values('╬ъЄ сЁ№ёър  58','┬шЄхсёъ','+375(29)230-11-71');
-insert into branch (street, city, tel_no)values('╬ъЄ сЁ№ёър  102','├Ёюфэю','+375(29)255-11-71');
-end;
+CREATE OR REPLACE TRIGGER SEQ_BRANCH BEFORE
+    INSERT ON BRANCH FOR EACH ROW
+BEGIN
+    SELECT
+        BRANCH_SEQ.NEXTVAL INTO :NEW.BNO
+    FROM
+        DUAL;
+END SEQ_BRANCH;
+
+BEGIN
+    INSERT INTO BRANCH(
+        STREET,
+        CITY,
+        TEL_NO
+    ) VALUES(
+        'Ленина 42',
+        'Минск',
+        '+375(29)221-11-71'
+    );
+    INSERT INTO BRANCH(
+        STREET,
+        CITY,
+        TEL_NO
+    ) VALUES(
+        'Якуба Коласа 13',
+        'Минск',
+        '+375(33)222-11-71'
+    );
+    INSERT INTO BRANCH(
+        STREET,
+        CITY,
+        TEL_NO
+    ) VALUES(
+        'Пролетарская 17',
+        'Брест',
+        '+375(25)223-11-71'
+    );
+    INSERT INTO BRANCH (
+        STREET,
+        CITY,
+        TEL_NO
+    )VALUES(
+        'Совецкая',
+        'Брест',
+        '+375(29)224-11-71'
+    );
+    INSERT INTO BRANCH (
+        STREET,
+        CITY,
+        TEL_NO
+    )VALUES(
+        'Комсомольская 764',
+        'Витебск',
+        '+375(29)225-11-71'
+    );
+    INSERT INTO BRANCH (
+        STREET,
+        CITY,
+        TEL_NO
+    )VALUES(
+        'Янки Купалы 123',
+        'Гомель',
+        '+375(29)226-11-71'
+    );
+    INSERT INTO BRANCH(
+        STREET,
+        CITY,
+        TEL_NO
+    ) VALUES(
+        'Максима Танка 64',
+        'Гродно',
+        '+375(29)227-11-71'
+    );
+    INSERT INTO BRANCH (
+        STREET,
+        CITY,
+        TEL_NO
+    )VALUES(
+        'Ленина 88',
+        'Могилев',
+        '+375(29)228-11-71'
+    );
+    INSERT INTO BRANCH (
+        STREET,
+        CITY,
+        TEL_NO
+    )VALUES(
+        'Машерова 44',
+        'Могилев',
+        '+375(33)229-11-71'
+    );
+    INSERT INTO BRANCH (
+        STREET,
+        CITY,
+        TEL_NO
+    )VALUES(
+        'Октябрьская 58',
+        'Витебск',
+        '+375(29)230-11-71'
+    );
+    INSERT INTO BRANCH (
+        STREET,
+        CITY,
+        TEL_NO
+    )VALUES(
+        'Октябрьская 102',
+        'Гродно',
+        '+375(29)255-11-71'
+    );
+END;
 /
 
-insert into staff values(staff_seq.nextval,'▀ъєсют','┬шъЄюЁ','╠шэёъ, ├юЁ№ъюую 19','+375(25)225-11-71','director',
-'male', to_date('17.03.86', 'dd.mm.yy'),300,1);
-insert into staff values(staff_seq.nextval,'▀ъєсютр','└ээр','┴ЁхёЄ, ├юЁ№ъюую 19','+375(29)225-11-71','manager',
-'female', to_date('22.09.87', 'dd.mm.yy'),250,2);
-insert into staff values(staff_seq.nextval,'╪хтЎют','─ьшЄЁшщ','├юьхы№, ╩ЁрёэюрЁьхщёър  14','+375(33)225-11-71','seller',
-'male', to_date('25.04.90', 'dd.mm.yy'),800,2);
-insert into staff values(staff_seq.nextval,'╩рЁях°','┼ышчртхЄр','├Ёюфэю, ╧ЁшЄ√Ўъюую 33','+375(29)221-11-71','manager',
-'female', to_date('30.12.91', 'dd.mm.yy'),200,3);
-insert into staff values(staff_seq.nextval,'╤рьёюэютр','─рЁ№ ','╠юушыхт, ╤ютхЄёър  12','+375(29)225-12-71','manager',
-'female', to_date('30.08.78', 'dd.mm.yy'),280,5);
-insert into staff values(staff_seq.nextval,'╪юёЄръ','╨єёырэ','╠шэёъ, ╦хэшэр 13','+375(29)225-31-71','manager',
-'male', to_date('04.05.96', 'dd.mm.yy'),190,6);
-insert into staff values(staff_seq.nextval,'╠шэшў','┬шъЄюЁ','├Ёюфэю, ╒ыхсюсєыюўэр  33','+375(29)225-41-71','seller',
-'male', to_date('12.08.88', 'dd.mm.yy'),700,6);
-insert into staff values(staff_seq.nextval,'╧юЄряхэъю','╠ръёшь','╠шэёъ, ┴хёяю∙рфэ  19','+375(29)225-51-71','seller',
-'male', to_date('25.06.80', 'dd.mm.yy'),900,7);
-insert into staff values(staff_seq.nextval,'╩хэфр','▀эр','┬шЄхсёъ, ╧ЁюыхЄрЁёър  73','+375(29)225-22-71','seeller',
-'male', to_date('17.03.86', 'dd.mm.yy'),300,8);
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Якубов',
+    'Виктор',
+    'Минск, Горького 19',
+    '+375(25)225-11-71',
+    'director',
+    'male',
+    TO_DATE('17.03.86', 'dd.mm.yy'),
+    300,
+    1
+);
 
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Якубова',
+    'Анна',
+    'Брест, Горького 19',
+    '+375(29)225-11-71',
+    'manager',
+    'female',
+    TO_DATE('22.09.87', 'dd.mm.yy'),
+    250,
+    2
+);
 
-insert into owner(ono,fname,lname,address,tel_no) select staff_seq.nextval,fname,lname,address,tel_no from staff;
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Шевцов',
+    'Дмитрий',
+    'Гомель, Красноармейская 14',
+    '+375(33)225-11-71',
+    'seller',
+    'male',
+    TO_DATE('25.04.90', 'dd.mm.yy'),
+    800,
+    2
+);
 
-begin
-insert into objects values (property_for_rent_seq.nextval,'▀ъєср ╩юырёр 43','╠шэёъ','h',2,600,12,3,1);
-insert into objects values (property_for_rent_seq.nextval,'╧ЁюыхЄрЁёър  13','╠шэёъ','f',3,500,13,5,1);
-insert into objects values (property_for_rent_seq.nextval,'╤ютхЎър  77','┴ЁхёЄ','f',4,100,14,3,3);
-insert into objects values (property_for_rent_seq.nextval,'▀эъш ъєяры√ 105','├юьхы№','h',4,400, 14,6,6);
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Карпеш',
+    'Елизавета',
+    'Гродно, Притыцкого 33',
+    '+375(29)221-11-71',
+    'manager',
+    'female',
+    TO_DATE('30.12.91', 'dd.mm.yy'),
+    200,
+    3
+);
 
-insert into renter values(renter_seq.nextval,'╚трэют','╚трэ','╠шэёъ, ╠шэшэр 5','+375(29)225-11-71','h',500,1);
-insert into renter values(renter_seq.nextval,'╧хЄЁют','╧хЄЁ','┴ЁхёЄ, ╠рыюЁшщёър  76','+375(29)225-11-72','f',400,1);
-insert into renter values(renter_seq.nextval,'╩юч ъ','╚эюъхэЄшщ','├Ёюфэю, ╧ЁюыхЄрЁёър  666','+375(29)225-11-73','h',900,3);
-insert into renter values(renter_seq.nextval,'└ыхъёххт','╠ръёшь','╠юушыхт, ┴юыюЄэр  9','+375(29)225-11-74','f',700,3);
-insert into renter values(renter_seq.nextval,'╨юьрэют','╨юьрэ','├юьхы№, ╤ютхЄёър  73','+375(29)225-11-75','h',500,6);
-end;
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Самсонова',
+    'Дарья',
+    'Могилев, Советская 12',
+    '+375(29)225-12-71',
+    'manager',
+    'female',
+    TO_DATE('30.08.78', 'dd.mm.yy'),
+    280,
+    5
+);
+
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Шостак',
+    'Руслан',
+    'Минск, Ленина 13',
+    '+375(29)225-31-71',
+    'manager',
+    'male',
+    TO_DATE('04.05.96', 'dd.mm.yy'),
+    190,
+    6
+);
+
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Минич',
+    'Виктор',
+    'Гродно, Хлебобулочная 33',
+    '+375(29)225-41-71',
+    'seller',
+    'male',
+    TO_DATE('12.08.88', 'dd.mm.yy'),
+    700,
+    6
+);
+
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Потапенко',
+    'Максим',
+    'Минск, Беспощадня 19',
+    '+375(29)225-51-71',
+    'seller',
+    'male',
+    TO_DATE('25.06.80', 'dd.mm.yy'),
+    900,
+    7
+);
+
+INSERT INTO STAFF VALUES(
+    STAFF_SEQ.NEXTVAL,
+    'Кенда',
+    'Яна',
+    'Витебск, Пролетарская 73',
+    '+375(29)225-22-71',
+    'seeller',
+    'male',
+    TO_DATE('17.03.86', 'dd.mm.yy'),
+    300,
+    8
+);
+
+INSERT INTO OWNER(
+    ONO,
+    FNAME,
+    LNAME,
+    ADDRESS,
+    TEL_NO
+)
+    SELECT
+        STAFF_SEQ.NEXTVAL,
+        FNAME,
+        LNAME,
+        ADDRESS,
+        TEL_NO
+    FROM
+        STAFF;
+
+BEGIN
+    INSERT INTO OBJECTS VALUES (
+        PROPERTY_FOR_RENT_SEQ.NEXTVAL,
+        'Якуба Коласа 43',
+        'Минск',
+        'h',
+        2,
+        600,
+        12,
+        3,
+        1
+    );
+    INSERT INTO OBJECTS VALUES (
+        PROPERTY_FOR_RENT_SEQ.NEXTVAL,
+        'Пролетарская 13',
+        'Минск',
+        'f',
+        3,
+        500,
+        13,
+        5,
+        1
+    );
+    INSERT INTO OBJECTS VALUES (
+        PROPERTY_FOR_RENT_SEQ.NEXTVAL,
+        'Совецкая 77',
+        'Брест',
+        'f',
+        4,
+        100,
+        14,
+        3,
+        3
+    );
+    INSERT INTO OBJECTS VALUES (
+        PROPERTY_FOR_RENT_SEQ.NEXTVAL,
+        'Янки купалы 105',
+        'Гомель',
+        'h',
+        4,
+        400,
+        14,
+        6,
+        6
+    );
+    INSERT INTO RENTER VALUES(
+        RENTER_SEQ.NEXTVAL,
+        'Иванов',
+        'Иван',
+        'Минск, Минина 5',
+        '+375(29)225-11-71',
+        'h',
+        500,
+        1
+    );
+    INSERT INTO RENTER VALUES(
+        RENTER_SEQ.NEXTVAL,
+        'Петров',
+        'Петр',
+        'Брест, Малорийская 76',
+        '+375(29)225-11-72',
+        'f',
+        400,
+        1
+    );
+    INSERT INTO RENTER VALUES(
+        RENTER_SEQ.NEXTVAL,
+        'Козяк',
+        'Инокентий',
+        'Гродно, Пролетарская 666',
+        '+375(29)225-11-73',
+        'h',
+        900,
+        3
+    );
+    INSERT INTO RENTER VALUES(
+        RENTER_SEQ.NEXTVAL,
+        'Алексеев',
+        'Максим',
+        'Могилев, Болотная 9',
+        '+375(29)225-11-74',
+        'f',
+        700,
+        3
+    );
+    INSERT INTO RENTER VALUES(
+        RENTER_SEQ.NEXTVAL,
+        'Романов',
+        'Роман',
+        'Гомель, Советская 73',
+        '+375(29)225-11-75',
+        'h',
+        500,
+        6
+    );
+END;
 /
-begin
-insert into viewing values(1, 6,to_date('03.12.19', 'dd.mm.yy'),'ok');
-insert into viewing values(1, 5,to_date('03.10.20', 'dd.mm.yy'),'good');
-insert into viewing values(2, 3,to_date('15.08.20', 'dd.mm.yy'),'');
-insert into viewing values(4, 3,to_date('16.08.19', 'dd.mm.yy'),'bad');
-insert into viewing values(5, 4,to_date('04.12.20', 'dd.mm.yy'),'');
-end;
+
+BEGIN
+    INSERT INTO VIEWING VALUES(
+        1,
+        6,
+        TO_DATE('03.12.19', 'dd.mm.yy'),
+        'ok'
+    );
+    INSERT INTO VIEWING VALUES(
+        1,
+        5,
+        TO_DATE('03.10.20', 'dd.mm.yy'),
+        'good'
+    );
+    INSERT INTO VIEWING VALUES(
+        2,
+        3,
+        TO_DATE('15.08.20', 'dd.mm.yy'),
+        ''
+    );
+    INSERT INTO VIEWING VALUES(
+        4,
+        3,
+        TO_DATE('16.08.19', 'dd.mm.yy'),
+        'bad'
+    );
+    INSERT INTO VIEWING VALUES(
+        5,
+        4,
+        TO_DATE('04.12.20', 'dd.mm.yy'),
+        ''
+    );
+END;
 /
